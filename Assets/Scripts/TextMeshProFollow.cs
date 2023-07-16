a4516f0552b98d91f725441530f82e85e50c78bd
hightop0924 <hightop.dev.hk@gmail.com>
2023-07-16 19:51:00 +0900

    HP update

--- Assets/Scripts/TextMeshProFollow.cs
+++ Assets/Scripts/TextMeshProFollow.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class TextMeshProFollow : MonoBehaviour
 {
-    public Transform target;  // the transform of the player's head
     public float yOffset = 1.5f;  // the vertical offset from the player's head
 
     private void LateUpdate()
     {
         // update the position of the TextMeshPro object
-        // transform.position = target.position + Vector3.up * yOffset;
 
-        // update the rotation of the TextMeshPro object to face the camera
-        // transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
-        //     Camera.main.transform.rotation * Vector3.up);
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
+            Camera.main.transform.rotation * Vector3.up);
     }
 }
